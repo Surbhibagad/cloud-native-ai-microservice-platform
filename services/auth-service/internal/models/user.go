9f8d4fd4d394978b86be2ad7fa7cd794f9ea7112
Surbhi <surbhi.22310249@viit.ac.in>
2026-07-16 10:33:25 +0530

    Add user model

---
+++ services/auth-service/internal/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
