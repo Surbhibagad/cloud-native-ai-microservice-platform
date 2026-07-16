ee02214d73f50fd4ba15e455a032cee7a899675d
Surbhi <surbhi.22310249@viit.ac.in>
2026-07-16 10:46:24 +0530

    Add authentication service layer

---
+++ services/auth-service/internal/services/auth_service.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/Surbhibagad/cloud-native-ai-microservice-platform/services/auth-service/internal/models"
+	"github.com/Surbhibagad/cloud-native-ai-microservice-platform/services/auth-service/internal/repository"
+	"github.com/Surbhibagad/cloud-native-ai-microservice-platform/services/auth-service/internal/utils"
+)
+
+type AuthService struct {
+	UserRepo *repository.UserRepository
+}
+
+func NewAuthService(repo *repository.UserRepository) *AuthService {
+	return &AuthService{
+		UserRepo: repo,
+	}
+}
+
+func (s *AuthService) Register(req *models.RegisterRequest) error {
+
+	// Check if user already exists
+	_, err := s.UserRepo.GetUserByEmail(req.Email)
+
+	if err == nil {
+		return errors.New("user already exists")
+	}
+
+	// If the error is something other than "no rows", return it.
+	if err != pgx.ErrNoRows {
+		return err
+	}
+
+	// Hash the password
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		return err
+	}
+
+	// Create the user model
+	user := &models.User{
+		FullName: req.FullName,
+		Email:    req.Email,
+		Password: hashedPassword,
+	}
+
+	// Save to database
+	return s.UserRepo.CreateUser(user)
+}
